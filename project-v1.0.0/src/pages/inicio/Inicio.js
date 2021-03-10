7b9520f5e3f061714d5927855374a5deb003c97b
Lorran Oliveira <58430075+lorran-xo@users.noreply.github.com>
2021-03-09 22:28:48 -0300

    Infos na aba Inicio e tabela na aba Produtos
    Provavelmente aba Produtos será retirada e ficará a aba Estoque no lugar

--- project-v1.0.0/src/pages/inicio/Inicio.js
+++ project-v1.0.0/src/pages/inicio/Inicio.js
@@ -4,6 +4,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import { Icon, Image, Statistic, Segment } from 'semantic-ui-react'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,6 +12,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     marginRight: theme.spacing(2),
+
   },
 }));
 
@@ -18,13 +20,28 @@ function App() {
     const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <header className="App-header">
-      </header>
-       <Paper className={classes.paper}>
-        <MenuList>
-          <MenuItem>This is inicio</MenuItem>
-        </MenuList>
+    <div>
+      <Paper elevation="0" className={classes.paper}>
+        <Segment >
+          <Statistic.Group style={{marginLeft:"30%"}} size='tiny'>
+            <Statistic>
+              <Statistic.Value>R$ 1,600</Statistic.Value>
+              <Statistic.Label>Em compras</Statistic.Label>
+            </Statistic>
+            <Statistic>
+              <Statistic.Value>
+                R$ 3,200
+              </Statistic.Value>
+              <Statistic.Label>Em vendas</Statistic.Label>
+            </Statistic>
+            <Statistic>
+              <Statistic.Value>
+               12
+              </Statistic.Value>
+              <Statistic.Label>Produtos</Statistic.Label>
+            </Statistic>
+          </Statistic.Group>
+        </Segment>
       </Paper>
     </div>
   );
--- project-v1.0.0/src/pages/produtos/Produtos.js
+++ project-v1.0.0/src/pages/produtos/Produtos.js
@@ -4,6 +4,10 @@ import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import { Table, Button,
+  Header,
+  Segment,
+  TransitionablePortal } from 'semantic-ui-react'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,13 +22,47 @@ function App() {
     const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <header className="App-header">
-      </header>
-       <Paper className={classes.paper}>
-        <MenuList>
-          <MenuItem>This is produtos</MenuItem>
-        </MenuList>
+    <div>
+
+  {/*
+  portals: https://react.semantic-ui.com/addons/transitionable-portal/#types-portal
+  confirm dialog portals: https://react.semantic-ui.com/addons/confirm/
+  */}
+      <Paper elevation="0" className={classes.paper}>
+        <Table basic size='small' style={{margin:"2%"}}>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Id</Table.HeaderCell>
+              <Table.HeaderCell>Produto</Table.HeaderCell>
+              <Table.HeaderCell>Preço compra</Table.HeaderCell>
+              <Table.HeaderCell>Preço revenda</Table.HeaderCell>
+              <Table.HeaderCell>Quantidade</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>1527</Table.Cell>
+              <Table.Cell>Pão de forma</Table.Cell>
+              <Table.Cell>2,00</Table.Cell>
+              <Table.Cell>5,00</Table.Cell>
+              <Table.Cell>13</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>2347</Table.Cell>
+              <Table.Cell>Bolo de pote</Table.Cell>
+              <Table.Cell>7,00</Table.Cell>
+              <Table.Cell>16,50</Table.Cell>
+              <Table.Cell>25</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>9132</Table.Cell>
+              <Table.Cell>Manteiga</Table.Cell>
+              <Table.Cell>3,60</Table.Cell>
+              <Table.Cell>8,00</Table.Cell>
+              <Table.Cell>10</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </Paper>
     </div>
   );
