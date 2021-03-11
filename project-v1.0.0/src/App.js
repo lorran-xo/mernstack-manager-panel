90ebfa3778f11a75842521dae408e71b8e1223ca
Lorran Oliveira <58430075+lorran-xo@users.noreply.github.com>
2021-03-11 12:30:47 -0300

    Várias implementações
    Substituído aba Produtos por Estoque;
    Ícone da página agora é uma imagem;
    Adicionado botão na tabela de Estoque;
    Adicionado Dashboard na tela Inicial que mostrará dados principais de venda;

--- project-v1.0.0/src/App.js
+++ project-v1.0.0/src/App.js
@@ -1,41 +1,42 @@
 import React, { Component } from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 import Inicio  from './pages/inicio/Inicio';
-import Produtos from './pages/produtos/Produtos';
+//import Produtos from './pages/produtos/Produtos';
 import Caixa from './pages/caixa/Caixa';
 import Vendas from './pages/vendas/Vendas';
 import Estoque from './pages/estoque/Estoque';
 import Financeiro from './pages/financeiro/Financeiro';
 import Sobre from './pages/sobre/Sobre';
-import Grid from '@material-ui/core/Grid';
-import { Button } from 'semantic-ui-react'
-
+import { Button, Grid } from 'semantic-ui-react'
 //icons
-import { MdLocalGroceryStore} from 'react-icons/md';
-
+import HortiFrutiLogo from './images/hortiFrutiLogo.png';
 
 class AppShell extends Component {
   render() {
     return (
       <div>
-        <h1><MdLocalGroceryStore style={{width:'40px', height: '25px'}}/> 
-          <span style={{fontFamily:'Garamond'}}>MerceariaManager</span>
-        </h1>
-        <center style={{marginTop:'-3%', marginRight:"10%"}}>
-          <Button.Group horizontal style={{marginBottom:'4%'}}>
-            <Link to='/'> <Button>Início</Button> </Link>
-            <Link to='/produtos'><Button>Produtos</Button></Link>
-            <Link to='/caixa'><Button>Caixa</Button></Link>
-            <Link to='/vendas'><Button>Vendas</Button></Link>
-            <Link to='/estoque'><Button>Estoque</Button></Link>
-            <Link to='/financeiro'><Button>Financeiro</Button></Link>
-            <Link to='/sobre'><Button>Sobre</Button></Link>
-          </Button.Group>
-        </center>
+        <Grid columns={4}>
+          <Grid.Row>
+            <Grid.Column>
+              <img src={HortiFrutiLogo} alt="Horti-Fruti-Logo" style={{}}/>
+            </Grid.Column>
+            <Grid.Column>
+              <Button.Group horizontal style={{margin:"5%"}}>
+                <Link to='/'> <Button>Início</Button> </Link>
+                <Link to='/estoque'><Button>Estoque</Button></Link>
+                {/*<Link to='/produtos'><Button>Produtos</Button></Link>*/}
+                <Link to='/caixa'><Button>Caixa</Button></Link>
+                <Link to='/vendas'><Button>Vendas</Button></Link>
+                <Link to='/financeiro'><Button>Financeiro</Button></Link>
+                <Link to='/sobre'><Button>Sobre</Button></Link>
+              </Button.Group>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
 
         <Switch>
           <Route exact path='/' component={ Inicio } />
-          <Route path='/produtos' component={ Produtos } />
+          {/*<Route path='/produtos' component={ Produtos } />*/}
           <Route path='/caixa' component={ Caixa } />
           <Route path='/vendas' component={ Vendas } />
           <Route path='/estoque' component={ Estoque } />
--- project-v1.0.0/src/pages/estoque/Estoque.js
+++ project-v1.0.0/src/pages/estoque/Estoque.js
@@ -1,9 +1,15 @@
 import './../../App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import { GrAdd } from 'react-icons/gr';
+import { Table, Button,
+  Header,
+  Segment,
+  TransitionablePortal } from 'semantic-ui-react';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,14 +23,55 @@ const useStyles = makeStyles((theme) => ({
 function App() {
     const classes = useStyles();
 
+    const [count, setCount] = useState(0);
+
+    function handleClick(){
+      setCount(count+1);
+      console.log("Vezes que o botão foi clicado: ", count);
+    }
+
   return (
-    <div className={classes.root}>
-      <header className="App-header">
-      </header>
-       <Paper className={classes.paper}>
-        <MenuList>
-          <MenuItem>This is estoque</MenuItem>
-        </MenuList>
+    <div>
+  {/*
+  portals: https://react.semantic-ui.com/addons/transitionable-portal/#types-portal
+  confirm dialog portals: https://react.semantic-ui.com/addons/confirm/
+  */}
+      <Paper elevation="0" className={classes.paper}>
+        <Table basic size='small' style={{margin:"2%"}}>
+          <Table.Header>
+          <Button style={{margin:'3%'}} onClick={handleClick} icon={GrAdd}/>
+            <Table.Row>
+              <Table.HeaderCell>Id</Table.HeaderCell>
+              <Table.HeaderCell>Produto</Table.HeaderCell>
+              <Table.HeaderCell>Quantidade (kg)</Table.HeaderCell>
+              <Table.HeaderCell>Preço compra</Table.HeaderCell>
+              <Table.HeaderCell>Preço revenda (kg)</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>1527</Table.Cell>
+              <Table.Cell>Tomate</Table.Cell>
+              <Table.Cell>25</Table.Cell>
+              <Table.Cell>90,00</Table.Cell>
+              <Table.Cell>5,20</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>2347</Table.Cell>
+              <Table.Cell>Batata</Table.Cell>
+              <Table.Cell>40</Table.Cell>
+              <Table.Cell>120,00</Table.Cell>
+              <Table.Cell>4,50</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>9132</Table.Cell>
+              <Table.Cell>Banana</Table.Cell>
+              <Table.Cell>60</Table.Cell>
+              <Table.Cell>310,00</Table.Cell>
+              <Table.Cell>6,00</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </Paper>
     </div>
   );
